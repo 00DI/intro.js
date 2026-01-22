c43201432223bc61701c0043e64e83afbbe2cb19
Dime <bigboydimeji@gmail.com>
2026-01-22 21:57:50 +0100

    intro.js

--- intro.js
+++ intro.js
@@ -1,1 +1,21 @@
-console.log("Oladimeji, 23, Phython")
+console.log("starting...");
+
+
+const name = "Oladimeji";
+
+const age = 23;
+
+const IsAlive = true;
+
+const colors = [ 'blue', 'red'];
+
+console.log("hello", name);
+
+{
+    function greet(name) {
+        return "Hello" + " " + name;
+}
+console.log(greet("Dimeji"));
+}
+
+console.log("End of file");
